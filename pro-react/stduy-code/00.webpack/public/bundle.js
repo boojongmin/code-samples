64d8f42f23e920259260f4b2a36f1634eb6e12c9
james.boo <boojongmin@daum.net>
2016-07-25 07:35:44 +0900

    p397 소스맵 생성

--- pro-react/stduy-code/00.webpack/public/bundle.js
+++ pro-react/stduy-code/00.webpack/public/bundle.js
@@ -44,19 +44,13 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	var greeter = __webpack_require__(1);
-	document.getElementById('root').appendChild(greeter());
-
+	eval("var greeter = __webpack_require__(1);\ndocument.getElementById('root').appendChild(greeter());\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvbWFpbi5qcz82YTRiIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0EiLCJmaWxlIjoiMC5qcyIsInNvdXJjZXNDb250ZW50IjpbInZhciBncmVldGVyID0gcmVxdWlyZSgnLi9HcmVldGVyLmpzJyk7XG5kb2N1bWVudC5nZXRFbGVtZW50QnlJZCgncm9vdCcpLmFwcGVuZENoaWxkKGdyZWV0ZXIoKSk7XG5cblxuXG4vKioqKioqKioqKioqKioqKipcbiAqKiBXRUJQQUNLIEZPT1RFUlxuICoqIC4vYXBwL21haW4uanNcbiAqKiBtb2R1bGUgaWQgPSAwXG4gKiogbW9kdWxlIGNodW5rcyA9IDBcbiAqKi8iXSwic291cmNlUm9vdCI6IiJ9");
 
 /***/ },
 /* 1 */
 /***/ function(module, exports) {
 
-	module.exports = function() {
-	    var greet = document.createElement('div');
-	    greet.textContent = 'Hi there and greetings!';
-	    return greet;
-	};
+	eval("module.exports = function() {\n    var greet = document.createElement('div');\n    greet.textContent = 'Hi there and greetings!';\n    return greet;\n};//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9hcHAvR3JlZXRlci5qcz9lYjg0Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiMS5qcyIsInNvdXJjZXNDb250ZW50IjpbIm1vZHVsZS5leHBvcnRzID0gZnVuY3Rpb24oKSB7XG4gICAgdmFyIGdyZWV0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgnZGl2Jyk7XG4gICAgZ3JlZXQudGV4dENvbnRlbnQgPSAnSGkgdGhlcmUgYW5kIGdyZWV0aW5ncyEnO1xuICAgIHJldHVybiBncmVldDtcbn07XG5cblxuLyoqKioqKioqKioqKioqKioqXG4gKiogV0VCUEFDSyBGT09URVJcbiAqKiAuL2FwcC9HcmVldGVyLmpzXG4gKiogbW9kdWxlIGlkID0gMVxuICoqIG1vZHVsZSBjaHVua3MgPSAwXG4gKiovIl0sInNvdXJjZVJvb3QiOiIifQ==");
 
 /***/ }
 /******/ ]);
