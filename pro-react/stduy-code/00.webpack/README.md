7e2f64116264b2306107099a68535454d66d25ff
james.boo <boojongmin@daum.net>
2016-07-25 20:28:24 +0900

    p403 설치와 구성 p406 바벨 구성 파일 p407 자바스크립트 외의 애셋 ~ p409

--- pro-react/stduy-code/00.webpack/README.md
+++ pro-react/stduy-code/00.webpack/README.md
@@ -1,17 +1,4 @@
-```shell
-npm install --save-dev babel-core babel-loader babel-preset-es2015 babel-preset-react
 ```
-
-```shell
-npm install --save react react-dom
+npm install --save-dev style-loader css-loader
 ```
-
-```
-Greeter.js 수정시 인텔리J에서 오류가 나오는 경우
-
-Languages & Frameworks > Javascript
-  ->  JavaScript lanugage version
-    ->  ECMAScript 5.1 => JSX Harmony
-```
-
 
--- pro-react/stduy-code/00.webpack/webpack.config.js
+++ pro-react/stduy-code/00.webpack/webpack.config.js
@@ -14,11 +14,13 @@ module.exports = {
             {
                 test: /\.js$/,
                 exclude: /node_modules/,
-                loader: 'babel',
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css'
             }
+
         ]
     },
     devServer: {
