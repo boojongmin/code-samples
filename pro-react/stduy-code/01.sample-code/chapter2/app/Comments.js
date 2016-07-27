23e9a59827f60abea890db601d145c663cf671ae
james.boo <boojongmin@daum.net>
2016-07-27 11:17:59 +0900

    chapter 2 샘플 소스

---
+++ pro-react/stduy-code/01.sample-code/chapter2/app/Comments.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Comments extends Component {
+    render() {
+        return (
+            <div>
+                {/* put {} around comments */ }
+                <span>This component has comments</span>
+            </div>
+        );
+    }
+}
+
+export default Comments
